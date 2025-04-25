0b7d88c4b9051199053221c2dc26e2b5e8188991
KySJ <smithkyra133@gmail.com>
2025-04-25 09:42:49 -0500

    Recursive string practice

--- recursive-func.js
+++ recursive-func.js
@@ -1,3 +1,9 @@
 function reverseString(string) {
-
+if (string.length <= 1) {
+    return string;
 }
+return string[string.length - 1] + reverseString(string.slice(0, string.length - 1));
+}
+
+console.log(reverseString("Disney"));
+console.log(reverseString("yensiD"));
